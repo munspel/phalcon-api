f125625ee3f11f67cb692770ba3840760ae77d22
L. Andrey <munspel@ukr.net>
2018-03-08 11:28:04 +0200

    Fix for proxy

---
+++ src/models/Jobs.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Models;
+
+use Phalcon\Mvc\Model;
+
+class Jobs extends Model
+{
+
+}
--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+use Phalcon\Mvc\Model\Manager as ModelsManager;
 use Phalcon\Http\Response;
 use Models\Workers;
 
@@ -16,23 +17,48 @@ $loader->registerNamespaces(
         ]
 );
 $loader->register();
-$di = new FactoryDefault();
+
+$di = new \Phalcon\DI\FactoryDefault();
+
 $di->set(
         'db', function () use ($db_config) {
-    return new PdoMysql($db_config);
-}
+            return new PdoMysql($db_config);
+        } 
 );
-$app = new Micro($di);
+$di->set(
+    "modelsManager", function() {
+        return new ModelsManager();
+    }
+);
 
+$app = new Micro($di);
 // Controllers
 $app->get('/v1/workers', function () {
     $countries = Workers::find();
+    
     $response = new Response();
+    $response->setHeader('Access-Control-Allow-Origin', '*');
+    $response->setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
     $response->setStatusCode(200, 'Found');
     $response->setJsonContent(
             [
                 'status' => 'OK',
                 'data' => $countries,
+            ]
+    );
+    return $response;
+});
+$app->get('/v1/workers/statuses', function () use ($app) {
+    $res = $app->modelsManager->executeQuery("SELECT type, status, worker, count(id) as count, max(last_update) as updated from Models\Jobs  group by type, status, worker");
+    
+    $response = new Response();
+    $response->setHeader('Access-Control-Allow-Origin', '*');
+    $response->setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+    $response->setStatusCode(200, 'Found');
+    $response->setJsonContent(
+            [
+                'status' => 'OK',
+                'data' => $res,
             ]
     );
     return $response;
